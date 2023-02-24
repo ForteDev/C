437e79d57330767a571d6ff7543d3021f022d938
ForteDev <whdtjd419@naver.com>
2023-02-24 18:10:11 +0900

    PN1152 문제풀이 해결완료

--- PN1152 copy.c
+++ PN1152 copy.c
@@ -4,17 +4,11 @@
 int main()
 {
     int count = 0;
-    char str[1000000];
+    char str[1000010];
     char *token;
 
     fgets(str,sizeof(str),stdin);
-
-    // printf("the last charater of str is %c\n",str[strlen(str)-2]);
     str[strlen(str)-1] = '\0';
-    // if(str[strlen(str)-1] == ' ')
-    // {
-    //     count--;
-    // }
 
     token = strtok(str, " ");
     while(token != NULL)
