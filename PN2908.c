f299fd28856e581fe879f148dd01da49127a4e0d
ForteDev <whdtjd419@naver.com>
2023-03-02 09:34:34 +0900

    PN2908 문제 풀이 완료

--- PN2908.c
+++ PN2908.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+char* mystrrev(char* str);
 
 int main()
 {
     char A[10], B[10];
+
     scanf("%s %s", A, B);
-    printf("A: %s, B: %s\n", A, B);
 
     char* revA = mystrrev(A);
     char* revB = mystrrev(B);
-
-    printf("reversed A: %s, reversed B: %s\n", revA, revB);
 
     int num1, num2;
     num1 = atoi(revA);
     num2 = atoi(revB);
 
-    printf("num1: %d, num2: %d\n", num1, num2);
     if(num1 > num2)
     {
         printf("%d", num1);
@@ -30,7 +28,7 @@ int main()
 char* mystrrev(char* str)
     {
     int len = strlen(str);
-    char* reversed_str[len];
+    char* reversed_str = (char *)malloc(sizeof(char) * len);
 
     for(int i = 0; i < len; i++)
     {
