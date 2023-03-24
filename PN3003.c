531d795eb1b65d3301ead2514715ec7e1874bbee
ForteDev <whdtjd419@naver.com>
2023-03-24 17:53:44 +0900

    PN3003 문제풀이

--- PN3003.c
+++ PN3003.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
     int white_pieces[6];
     int standard_pieces[6] = { 1, 1, 2, 2, 2, 8 };
     char input_nums[20];
-    scanf("%s", input_nums);
-    printf("%s", input_nums);
+    char* token;
+    
+    fgets(input_nums, sizeof(input_nums),stdin);
+    
+    token = strtok(input_nums, " ");
 
+    for(int i = 0; i < 6; i++)
+    {
+        white_pieces[i] = atoi(token);
+        printf("%d ", standard_pieces[i] - white_pieces[i]);
+        token = strtok(NULL, " ");
+    }
+
+
+    return 0;
 }
